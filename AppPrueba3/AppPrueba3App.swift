609986d13b2520f9cf85f2c9cad9e2def8d9fce0
rvmiranda <rafmicr884@gmail.com>
2025-11-18 14:38:03 -0600

    Initial Commit

---
+++ AppPrueba3/AppPrueba3App.swift
@@ -0,0 +1,17 @@
+//
+//  AppPrueba3App.swift
+//  AppPrueba3
+//
+//  Created by Rafael Miranda on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPrueba3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPrueba3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPrueba3
+//
+//  Created by Rafael Miranda on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
